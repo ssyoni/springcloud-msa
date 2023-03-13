e69d1445085a24f45d0ef28cb2e2dfa6171e572a
ssyoni <jsy950601@gmail.com>
2023-03-13 20:27:53 +0900

    Initial commit

---
+++ user-service/src/main/java/com/msa/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.msa.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/healthcheck")
+    public String healthCheck(){
+        return "health ok";
+    }
+
+}
